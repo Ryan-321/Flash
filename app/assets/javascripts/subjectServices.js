7480994f074f66983f22820fa1c5c8fa576922ac
Ryan Kahle <jrkahle@hotmail.com>
2015-09-18 09:16:17 -0400

    added s to http

--- app/assets/javascripts/subjectServices.js
+++ app/assets/javascripts/subjectServices.js
@@ -2,13 +2,19 @@
   var subjectServices = angular.module('subjectServices', ['ngResource']);
 
   subjectServices.factory('Subject', ['$resource', function($resource) {
-    return $resource('http://wdi-flash.herokuapp.com/subjects/:id.json', {}, {
+    return $resource('https://wdi-flash.herokuapp.com/subjects/:id.json', {}, {
       update: {method:'PUT'} // Medthod that needs to be added b/c $resouce doesn't support PUT, which we need for Editing
     });
   }]);
   subjectServices.factory('Card', ['$resource', function($resource) {
-    return $resource('http://wdi-flash.herokuapp.com/subjects/:subjectId/cards/:id', {}, {
+    return $resource('https://wdi-flash.herokuapp.com/subjects/:subjectId/cards/:id', {}, {
       update: {method:'PUT'}
     });
   }]);
 })()
+
+// localhost:3000 is local url
+
+// remember to change the s to test in development
+
+// use when pushing up to heroku, https://wdi-flash.herokuapp.com
