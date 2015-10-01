79a0c42b0b1a76c0bca56a5b75a010b6582c5ee5
Ryan Kahle <jrkahle@hotmail.com>
2015-10-01 00:19:04 -0400

    added cards

--- app/assets/javascripts/subjectsControllers.js
+++ app/assets/javascripts/subjectsControllers.js
@@ -5,15 +5,12 @@
  // index controller
   subjectsControllers.controller('subjectsController', ['Subject', function(Subject) {
       this.subjects = Subject.query(function(data){
-        console.log(data) // checking to see what returning in Subject.query();
       });
     }]);
 
   // new controller (will create the subjects)
   subjectsControllers.controller('newSubjectsController',['$location','Subject', function($location, Subject){
     this.create = function() {
-      console.log("new fired")
-      console.log(this.subject)
       Subject.save(this.subject, function(subject){ // only need subject if I want to pass in ID to URL
         $location.path("/subjects/")
       })
@@ -27,30 +24,26 @@
     }
   )
     this.delete = function(id){
-      console.log("This workss")
         Subject.delete({id: id}, function(){
 	      $location.path("/subjects")
       });
     }
     this.createCard = function(card){
       var self = this
-      Card.save({subjectId: $routeParams.id}, card, function(card){
-        self.subject.cards.push(self.card)
-        self.card = {}, function() {
-          $location.path("/subjects/" + self.card.id)
-        }
+      Card.save({subjectId: $routeParams.id}, self.card, function(card){
+        self.subject.cards.push(self.card),
+        self.card = {}
       })
+      $location.path("/subjects/" + this.subject.id)
     }
     this.deleteCard = function(card) {
       var self = this;
-      console.log(card)
       Card.delete({subjectId: this.subject.id, id: card.id}, function() {
         self.subject.cards = Card.query({subjectId: self.subject.id});
       });
     }
     this.editCard = function(card) {
       this.card = Card.get({subjectId: $routeParams.id, id: card.id})
-      console.log(card)
       this.update = function() {
         this.card.update({id: this.subject.id})
       }
